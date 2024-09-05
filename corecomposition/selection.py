fixed simulation averaging

corecomposition/selection.py
@@ -120,7 +120,7 @@ def radius_from_cmd(catalog, params):
                                 HR_bands=('bp3-rp3', 'G3'))
 
     bp3_rp3 = catalog['bpmag_dereddened'] - catalog['rpmag_dereddened']
-    G3 = catalog['gmag_dereddened'] + 5 * (np.log10(catalog['r_med_geo'] / 100))
+    G3 = catalog['gmag_dereddened'] - 5 * np.log10(catalog['r_med_geo']) + 5
     logg = model['HR_to_logg'](bp3_rp3, G3)
     mass = model['HR_to_mass'](bp3_rp3, G3)
 
@@ -132,12 +132,12 @@ def deredden_gaia(catalog, bsq):
     photo = np.array([catalog['wd_phot_g_mean_mag'], catalog['wd_phot_bp_mean_mag'], catalog['wd_phot_rp_mean_mag']]).T # the basic Gaia photometry
     bands = ['Gaia_G', 'Gaia_BP', 'Gaia_RP']
 
-    photo_dereddened = np.array([])
-    for i in range(len(geometry)):
-        result = utils.deredden(bsq, geometry, photo, bands)
-        photo_dereddened = np.append(photo_dereddened, result)
+    photo_dereddened = []
+    for i in tqdm(range(len(geometry))):
+        result = utils.deredden(bsq, geometry[i], photo[i], bands)
+        photo_dereddened.append(result)
+    photo_dereddened = np.array(photo_dereddened)
 
-    print(photo_dereddened)
     catalog['gmag_dereddened'] = photo_dereddened[:,0]
     catalog['bpmag_dereddened'] = photo_dereddened[:,1]
     catalog['rpmag_dereddened'] = photo_dereddened[:,2]

corecomposition/vgsimulator.py
@@ -51,11 +51,13 @@ def simulate(wl, n_sims, snr_grid, R, teff, distance):
     teffs = []
     dists = []
     radii = []
-    rvs = []
-    measured_rvs = []
-    e_rvs = []
+    delta_rv = []
 
     for ii, snr in enumerate(tqdm(snr_grid)):
+        rvs = []
+        measured_rvs = []
+        e_rvs = []
+
         for jj in range(n_sims):
             radius = np.random.uniform(low = 0.004, high = 0.0065)
             rv = np.random.uniform(low = -50, high = 50)
@@ -65,21 +67,27 @@ def simulate(wl, n_sims, snr_grid, R, teff, distance):
             measured_rv, e_rv, redchi, param_res = corv.fit.fit_corv(wl_fetched, fl, ivar, corvmodel)
             #corv.utils.lineplot(wl_fetched, fl, ivar, corvmodel, param_res.params, printparams = False, gap = 0.3, figsize = (6, 5))
 
-            snrs.append(snr)
-            teffs.append(teff)
-            dists.append(distance)
-            radii.append(radius)
             rvs.append(rv)
+            e_rvs.append(e_rv)
             measured_rvs.append(measured_rv)
-            e_rvs.append(np.sqrt(e_rv**2 + (measured_rv - rv)**2))
+
+        snrs.append(snr)
+        teffs.append(teff)
+        dists.append(distance)
+        radii.append(radius)
+        rvs = np.array(rvs)
+        e_rvs = np.array(e_rvs)
+        measured_rvs = np.array(measured_rvs)
+
+        delta_rv.append(np.sqrt(np.mean(e_rv)**2 + (np.mean(measured_rvs - rvs))**2))
 
     parameters = Table()
     parameters['snr'] = snrs
     parameters['teff'] = teffs
     parameters['distance'] = dists
-    parameters['radius'] = radii
-    parameters['rv'] = rvs
-    parameters['measured_rv'] = measured_rvs
-    parameters['measured_e_rv'] = e_rvs
+    #parameters['radius'] = radii
+    #parameters['rv'] = rvs
+    #parameters['measured_rv'] = measured_rvs
+    parameters['measured_e_rv'] = delta_rv
 
     return parameters